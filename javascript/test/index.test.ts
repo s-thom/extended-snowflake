2b83ab25845a5cf9d1c87f2d761cb2af543bd370
Stuart Thomson <me@sthom.kiwi>
2019-08-10 12:04:51 +1200

    Add worker/process ID range checks

--- javascript/test/index.test.ts
+++ javascript/test/index.test.ts
@@ -6,3 +6,27 @@ test('Worker and Process IDs combine', () => {
   expect(combineWorkerProcess(31, 31)).toBe(0x3FF); // 11111 11111
   expect(combineWorkerProcess(5, 19)).toBe(0x0B3); // 00101 10011
 });
+
+test('Worker ID range too low', () => {
+  expect(() => {
+    combineWorkerProcess(-1, 0);
+  }).toThrow();
+});
+
+test('Worker ID range too high', () => {
+  expect(() => {
+    combineWorkerProcess(32, 0);
+  }).toThrow();
+});
+
+test('Process ID range too low', () => {
+  expect(() => {
+    combineWorkerProcess(0, -1);
+  }).toThrow();
+});
+
+test('Process ID range too high', () => {
+  expect(() => {
+    combineWorkerProcess(0, 32);
+  }).toThrow();
+});
